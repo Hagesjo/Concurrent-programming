e134eca929683f674ba13a47b4810979963cf768
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2014-01-29 15:14:34 +0100

    evolved the sensors a bit

--- Trainspotting/src/Lab1.java
+++ Trainspotting/src/Lab1.java
@@ -1,9 +1,11 @@
 import TSim.*;
 import java.util.concurrent.*;
+import java.util.HashMap;
 
 public class Lab1 {
    private final int MAXSPEED = 15;
    private int simulationspeed = 100;
+   private HashMap<Integer, Semaphore> allSensors = new HashMap<>();
 
    public static void main(String[] args) {
       new Lab1(args);
@@ -12,34 +14,37 @@ public class Lab1 {
    public Lab1(String[] args) {
       Semaphore[] semaphores = new Semaphore[10];
 
-      for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < 9; i++) {
          semaphores[i] = new Semaphore(1,true);
+         
       }
 
       TSimInterface tsi = TSimInterface.getInstance();
-      tsi.setDebug(true);
+      TSimStream  tstr = new TSimStream(System.in);
+      tsi.setDebug(false);
 
       Train train1 = new Train(1, semaphores); 
       Train train2 = new Train(2, semaphores);
-      if (args[1] != null) { 
+      if (args.length == 3 ) { 
          train1.setSpeed(Integer.parseInt(args[0]));
          train2.setSpeed(Integer.parseInt(args[1]));
-         simulationspeed = Integer.parseInt(args[4]);
+         simulationspeed = Integer.parseInt(args[2]);
       } else {
-         train1.setSpeed((int) Math.random()*MAXSPEED);
-         train2.setSpeed((int) Math.random()*MAXSPEED);
+         train1.setSpeed((int) (Math.random()*MAXSPEED));
+         train2.setSpeed((int) (Math.random()*MAXSPEED));
          simulationspeed = Integer.parseInt(args[0]);
       }
-      //try {
-           //tsi.setDebug(true);
-           //tsi.setSpeed(1,10);
-           //tsi.setSpeed(2,10);
-      //}
-      //catch (CommandException e) {
-         //e.printStackTrace();    // or only e.getMessage() for the error
-         //System.exit(1);
-      //}
+      while(true) {
+         try {
+            SensorEvent olol=(SensorEvent) tstr.read();
+            System.err.println(olol.getXpos());
+         }
+         catch (UnparsableInputException e) {
+            System.err.println("OLOLOLOL");
+         }
+      }
    }
+
    public class Train extends Thread implements Runnable {
       private Semaphore[] rails;
       public int speed;
@@ -57,6 +62,7 @@ public class Lab1 {
          } 
          catch (CommandException e) {
             System.err.println("Error setting speed");
+            System.exit(1);
          }
       }
    }
