c879b51c8cb4ef953f97ba5a5cc8476588a5abc7
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2014-02-02 19:56:22 +0100

    Code cleanup and comments

--- Trainspotting/src/Lab1.java
+++ Trainspotting/src/Lab1.java
@@ -2,18 +2,21 @@ import TSim.*;
 import java.util.concurrent.*;
 import java.util.HashMap;
 
+   /** 
+    * @author Andreas Hagesjö och Robert Nyquist
+    */
 public class Lab1 {
-   private final int MAXSPEED = 19;
    private int simulationspeed = 100;
-   private final int down = 1;
+   private final int MAXSPEED = 19;
+   private final int down = 1; //constant variables to keep track of which direction the train has
    private final int up = -1;
    Semaphore[] semaphores = new Semaphore[6];
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws InterruptedException, CommandException{
       new Lab1(args);
    }
 
-   public Lab1(String[] args) {
+   public Lab1(String[] args) throws InterruptedException, CommandException {
       for (int i = 0; i < 6; i++) {
          semaphores[i] = new Semaphore(1,true);
       }
@@ -21,15 +24,18 @@ public class Lab1 {
       tsi.setDebug(false);
       Train train1 = new Train(1, down); 
       Train train2 = new Train(2, up);
-      if (args.length == 3 ) { 
+      
+      if (args.length >= 1) {
+         simulationspeed = Integer.parseInt(args[args.length - 1]);
+      }
+      if (args.length == 2) {
+         train1.initspeed = Integer.parseInt(args[0]);
+      }
+      if (args.length == 3) {
          train1.initspeed = Integer.parseInt(args[0]);
          train2.initspeed = Integer.parseInt(args[1]);
-         simulationspeed = Integer.parseInt(args[2]);
-      } else {
-         train1.initspeed = (int) (Math.random()*MAXSPEED);
-         train2.initspeed = (int) (Math.random()*MAXSPEED);
-         simulationspeed = Integer.parseInt(args[0]);
       }
+
       train1.setSpeed(train1.initspeed);
       train2.setSpeed(train2.initspeed);
       train1.start();
@@ -38,58 +44,65 @@ public class Lab1 {
 
    public class Train extends Thread {
       private int direction;
-      private int initspeed;
+      private int initspeed = (int) (Math.random()*(MAXSPEED - 1) + 1); 
       public int speed;
       public int id;
       public TSimInterface tsi;
       boolean turning = true;
 
-      public Train(int id, int direction) {
+         /**
+          * @param id The id of the train
+          * @param direction The direction of the train. 1 is down, -1 is up
+          */
+      public Train(int id, int direction) throws InterruptedException, CommandException{
          this.id = id;
          this.direction = direction;
          tsi = TSimInterface.getInstance();
-         try {
+            //Acquire the semaphore for the station the train starts at
             if (direction == down) {
                semaphores[3].acquire();
             } else {
                semaphores[1].acquire();
             }
-         }
-         catch (Exception e) {}
       }
       
-      public void setSpeed(int speed) {
+         /** 
+          * Set a new speed to the train
+          * @param speed the new speed of the train
+          */
+      public void setSpeed(int speed) throws CommandException{
          this.speed = speed;
-         try {
-            tsi.setSpeed(id,speed);
-         } 
-         catch (CommandException e) {
-            System.err.println("Error setting speed");
-            System.exit(1);
-         }
+         tsi.setSpeed(id,speed);
       }
-      private void turnTrain() {
+         /**
+          * Turns the train. 
+          * The function sets the speed to 0, then waits 2 seconds, and then
+          * sets the speed to the old speed (but backwards of course)
+          */
+      private void turnTrain() throws CommandException, InterruptedException {
          int tmpspeed = speed;
          setSpeed(0);
-         try {
-            sleep(2000 + 2 * simulationspeed * Math.abs(tmpspeed));
-         }
-         catch (InterruptedException e) {}
+         sleep(2000 + 2 * simulationspeed * Math.abs(tmpspeed));
          setSpeed(-tmpspeed);
          initspeed = -tmpspeed;
          direction = -direction;
          turning = false;
       }
 
-      private void waitUntilReady(int semaphoreId,int x, int y, int switchDirection) {
-         try {
+         /** 
+          * Makes the train wait until the next session is released. 
+          * When it is free to go, it also sets the switch in front of the
+          * train to the correct direction
+          * @param semaphoreId Semaphore which the train waits for to be released
+          * @param x The x-coordinate for the switch
+          * @param y The y-coordinate for the switch
+          * @param switchDirection The direction to turn the switch to
+          */
+      private void waitUntilReady(int semaphoreId,int x, int y, int switchDirection) throws InterruptedException, CommandException {
             setSpeed(0);
             semaphores[semaphoreId].acquire();
             setSpeed(initspeed);
             tsi.setSwitch(x,y, switchDirection);
-         } catch (Exception e) {
-            System.exit(1);
-         }
       }
 
       @Override
@@ -98,7 +111,8 @@ public class Lab1 {
             try {
                SensorEvent sens = tsi.getSensor(id);
                if (sens.getStatus() == 1) {
-                  switch (sens.getXpos() * 100 + sens.getYpos()) {
+                     //Every case (sensor) is described in dokumentation.txt
+                  switch (sens.getXpos() * 100 + sens.getYpos()) { //Makes a unique ID for every sensor
                      case 509:
                         if (direction == down) {
                            semaphores[4].release();
@@ -118,7 +132,7 @@ public class Lab1 {
                            semaphores[0].release();
                         }
                      break;
-                     case 1007: case 1008:
+                     case 1107: case 1008:
                         if (direction == up) {
                            setSpeed(0);
                            semaphores[0].acquire();
