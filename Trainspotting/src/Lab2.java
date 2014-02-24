00bfdb6bb1e9c11e5c13d05c617752f80b26837d
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2014-02-24 09:57:56 +0100

    Robert, du är fan sämst

--- Trainspotting/src/Lab2.java
+++ Trainspotting/src/Lab2.java
@@ -1,21 +1,20 @@
 import TSim.*;
 import java.util.concurrent.locks.*;
-import java.util.HashMap;
 
-   /** 
-    * @author Andreas Hagesjö och Robert Nyquist
-    */
+/** 
+ * @author Andreas Hagesjö och Robert Nyquist
+ */
 public class Lab2 {
    private int simulationspeed = 100;
    private final int MAXSPEED = 19;
    private final int down = 1; 
    private final int up = -1;
-   Monitor monitor0 = new Monitor();
-   Monitor monitor1 = new Monitor();
-   Monitor monitor2 = new Monitor();
-   Monitor monitor3 = new Monitor();
-   Monitor monitor4 = new Monitor();
-   Monitor monitor5 = new Monitor();
+   Track track0 = new Track();
+   Track track1 = new Track();
+   Track track2 = new Track();
+   Track track3 = new Track();
+   Track track4 = new Track();
+   Track track5 = new Track();
 
 
 
@@ -26,10 +25,10 @@ public class Lab2 {
    public Lab2(String[] args) throws InterruptedException, CommandException {
 
       TSimInterface tsi = TSimInterface.getInstance();
-      tsi.setDebug(true);
+      tsi.setDebug(false);
       Train train1 = new Train(1, down); 
       Train train2 = new Train(2, up);
-      
+
       if (args.length >= 1) {
          simulationspeed = Integer.parseInt(args[args.length - 1]);
       }
@@ -60,19 +59,19 @@ public class Lab2 {
          this.id = id;
          this.direction = direction;
          tsi = TSimInterface.getInstance();
-            if (direction == down) {
-               monitor3.occupated = false;
-            } else {
-               monitor1.occupated = false;
-            }
+         if (direction == down) {
+            track3.enter();
+         } else {
+            track1.enter();
+         }
       }
-      
+
 
       public void setSpeed(int speed) throws CommandException{
          this.speed = speed;
          tsi.setSpeed(id,speed);
       }
-  
+
 
       private void turnTrain() throws CommandException, InterruptedException {
          int tmpspeed = speed;
@@ -84,250 +83,234 @@ public class Lab2 {
          turning = false;
       }
 
-      private void waitUntilReady(Monitor track,int x, int y, int switchDirection) throws InterruptedException, CommandException {
-	    
-            setSpeed(0);
-            while(!track.tryEnter()){
-		}
-            setSpeed(initspeed);
-		track.enter();
-            tsi.setSwitch(x,y, switchDirection);
+      private void waitUntilReady(Track track,int x, int y, int switchDirection) throws InterruptedException, CommandException {
+         setSpeed(0);
+         track.enter();
+         setSpeed(initspeed);
+         tsi.setSwitch(x,y, switchDirection);
       }
 
       @Override
-      public void run() {
-            //We use 6 semaphores for the different areas.
-            //semaphores[0] is the cross at the top
-            //semaphores[1] is the bottom station
-            //semaphores[2] is the critical "onelinepath" after the bottom station 
-            //semaphores[3] is the top station
-            //semaphores[4] is the fastest path in the "choose-section" in the middle
-            //semaphores[5] is the critical "onelinepath" after the top station
+         public void run() {
+            //We use 6 Tracks for the different areas.
+            //track0 is the cross at the top
+            //track1 is the bottom station
+            //track2 is the critical "onelinepath" after the bottom station 
+            //track3 is the top station
+            //track4 is the fastest path in the "choose-section" in the middle
+            //track5 is the critical "onelinepath" after the top station
 
-            //Everytime the train has completely left a path connected to a semaphore, this semaphore is released, and the new semaphore (the path the train entered) is (if possible) acquired.
-            //Everytime the path (semaphore) the train is about to enter already is acquired, the train stops and waits for the semaphore to be released before starting again (exception below):
-            
-            //We use tryAcquire everytime the train has to choose path,
+            //Everytime the train has completely left a path connected to a Track, this Track is set to empty, and the new Track (the path the train entered) is (if possible) entered.
+            //Everytime the path (Track) the train is about to enter already is entered, the train stops and waits for the Track to be empty before starting again (exception below):
+
+            //We use tryEnter everytime the train has to choose path,
             //which means when the train is about to exit a critical path, and therefore doesn't have to stop, it just has to choose path.
-            //If the semaphore for the default path fails to be acquired, the train (well...the switch) simply chooses the other free path
-         while (true) {
-            try {
-               SensorEvent sens = tsi.getSensor(id);
-               if (sens.getStatus() == 1) {
-                  //All cases are sorted according to the map (easier reading)
-                  switch (sens.getXpos() * 100 + sens.getYpos()) { //Makes a unique ID for every sensor
-                     case 606: case 905:
-                        if (direction == down) {
-                           setSpeed(0);
-			   while(!monitor0.tryEnter()){
-				}
-                           setSpeed(initspeed);
-				monitor0.enter();
-                        } else {
-			   monitor0.leave();
-                        }
-                     break;
-
-                     case 1107: case 1008:
-                        if (direction == up) {
-                           setSpeed(0);
-			   while(!monitor0.tryEnter()){
-				}
-                           setSpeed(initspeed);
-                        } else {
-			   monitor5.leave();
-                        }
-                     break;
-
-                     case 1508:
-                        if (direction == down) {
-                           waitUntilReady(monitor5,17,7,1);
-                        } else {
-			   monitor5.leave();
-                        }
-                        break;
-
-                     case 1407:
-                        if (direction == down) {
-                           waitUntilReady(monitor5,17,7,2);
-			   monitor3.leave();
-                        } else {
-			   monitor5.leave();
-                        }
-                        break;
-
-                     case 1908:
-                      if (direction == up) {
-                           if (monitor3.tryEnter()) {
-                              tsi.setSwitch(17,7,2);
+            //If the Track for the default path fails to be entered, the train (well...the switch) simply chooses the other free path
+            while (true) {
+               try {
+                  SensorEvent sens = tsi.getSensor(id);
+                  if (sens.getStatus() == 1) {
+                     //All cases are sorted according to the map (easier reading)
+                     switch (sens.getXpos() * 100 + sens.getYpos()) { //Makes a unique ID for every sensor
+                        case 606: case 905:
+                           if (direction == down) {
+                              setSpeed(0);
+                              track0.enter();
+                              setSpeed(initspeed);
                            } else {
-                              tsi.setSwitch(17,7,1);
+                              track0.leave();
                            }
-                        }
-                        break;
-                        
-                     case 1709:
-                        if (direction == down) {
-                           if (monitor4.tryEnter()) {
-                              tsi.setSwitch(15,9,2);
-                           } else {
-                              tsi.setSwitch(15,9,1);
-                           }
-                        }
-                        break;
-
-                     case 1409:
-                        if (direction == up) {
-			   monitor4.leave();
-                        }
-                     break;
-
-                     case 1209:
-                        if (direction == up) {
-                           waitUntilReady(monitor5,15,9,2);
-                        } else {
-			   monitor5.leave();
-                        }
-                        break;
-
-                     case 1310:
-                        if (direction == up) {
-                           waitUntilReady(monitor5,15,9,1);
-                        } else {
-			   monitor5.leave();
-                        }
-                        break;
-
-                     case 709:
-                        if (direction == down) {
-                           waitUntilReady(monitor2,4,9,1);
-                        } else {
-			   monitor2.leave();
-                        }
-                        break;
-
-                     case 610:
-                        if (direction == down) {
-                           waitUntilReady(monitor2,4,9,2);
-                        } else {
-			   monitor2.leave();
-                        }
-                        break;
-
-                     case 509:
-                        if (direction == down) {
-			   monitor4.leave();
-                        }
-                     break;
-
-                     case 209:
-                    if (direction == up) {
-                           if (monitor4.tryEnter()) {
-                              tsi.setSwitch(4,9,1);
-                           } else {
-                              tsi.setSwitch(4,9,2);
-                           }
-                        } 
-                        break;
-
-                     case 110:
-                        if (direction == down) {
-                             if (monitor1.tryEnter()) {
-                              tsi.setSwitch(3,11,1);
-                           } else {
-                              tsi.setSwitch(3,11,2);
-                           }
-			}
                            break;
 
-                     case 413:
-                        if(direction == up){
-                           waitUntilReady(monitor2,3,11,2);
-                        } else {
-                           monitor2.leave();
-                        }
-                        break;
+                        case 1107: case 1008:
+                           if (direction == up) {
+                              setSpeed(0);
+                              track0.enter();
+                              setSpeed(initspeed);
+                           } else {
+                              track0.leave();
+                           }
+                           break;
 
-                     case 611:
-                        if(direction == up){
-                           waitUntilReady(monitor2,3,11,1);
-                           monitor1.leave();
-                        } else {
-			   monitor2.leave();
-                        }
-                        break;
+                        case 1508:
+                           if (direction == down) {
+                              waitUntilReady(track5,17,7,1);
+                           } else {
+                              track5.leave();
+                           }
+                           break;
 
-                        //Endstation-sensors. Makes the train reverse
-                     case 1411: case 1413: case 1403: case 1405:
-                        if (!turning) {
-                           turnTrain();
-                        } else {
-                           turning = false;
-                        }
-                        break;
+                        case 1407:
+                           if (direction == down) {
+                              waitUntilReady(track5,17,7,2);
+                              track3.leave();
+                           } else {
+                              track5.leave();
+                           }
+                           break;
+
+                        case 1908:
+                           if (direction == up) {
+                              if (track3.tryEnter()) {
+                                 tsi.setSwitch(17,7,2);
+                              } else {
+                                 tsi.setSwitch(17,7,1);
+                              }
+                           }
+                           break;
+
+                        case 1709:
+                           if (direction == down) {
+                              if (track4.tryEnter()) {
+                                 tsi.setSwitch(15,9,2);
+                              } else {
+                                 tsi.setSwitch(15,9,1);
+                              }
+                           }
+                           break;
+
+                        case 1409:
+                           if (direction == up) {
+                              track4.leave();
+                           }
+                           break;
+
+                        case 1209:
+                           if (direction == up) {
+                              waitUntilReady(track5,15,9,2);
+                           } else {
+                              track5.leave();
+                           }
+                           break;
+
+                        case 1310:
+                           if (direction == up) {
+                              waitUntilReady(track5,15,9,1);
+                           } else {
+                              track5.leave();
+                           }
+                           break;
+
+                        case 709:
+                           if (direction == down) {
+                              waitUntilReady(track2,4,9,1);
+                           } else {
+                              track2.leave();
+                           }
+                           break;
+
+                        case 610:
+                           if (direction == down) {
+                              waitUntilReady(track2,4,9,2);
+                           } else {
+                              track2.leave();
+                           }
+                           break;
+
+                        case 509:
+                           if (direction == down) {
+                              track4.leave();
+                           }
+                           break;
+
+                        case 209:
+                           if (direction == up) {
+                              if (track4.tryEnter()) {
+                                 tsi.setSwitch(4,9,1);
+                              } else {
+                                 tsi.setSwitch(4,9,2);
+                              }
+                           } 
+                           break;
+
+                        case 110:
+                           if (direction == down) {
+                              if (track1.tryEnter()) {
+                                 tsi.setSwitch(3,11,1);
+                              } else {
+                                 tsi.setSwitch(3,11,2);
+                              }
+                           }
+                           break;
+
+                        case 413:
+                           if(direction == up){
+                              waitUntilReady(track2,3,11,2);
+                           } else {
+                              track2.leave();
+                           }
+                           break;
+
+                        case 611:
+                           if(direction == up){
+                              waitUntilReady(track2,3,11,1);
+                              track1.leave();
+                           } else {
+                              track2.leave();
+                           }
+                           break;
+
+                           //Endstation-sensors. Makes the train reverse
+                        case 1411: case 1413: case 1403: case 1405:
+                           if (!turning) {
+                              turnTrain();
+                           } else {
+                              turning = false;
+                           }
+                           break;
+                     }
                   }
                }
-            }
-            catch (CommandException e) {
-               System.exit(1);
-            }
-            catch (InterruptedException e) {
-               System.exit(1);
+               catch (CommandException e) {
+                  System.exit(1);
+               }
+               catch (InterruptedException e) {
+                  System.exit(1);
+               }
             }
          }
+   }
+   public class Track {
+      private Lock lock = new ReentrantLock();
+      private Condition critical = lock.newCondition();
+      private boolean empty = true;
+
+      /**
+       * Enters a track.
+       * If the track is not empty, it will wait for the signal that the method "leave" sends
+       */
+      public void enter() throws InterruptedException {
+         lock.lock();
+         if (!empty) {
+            critical.await();
+         }
+         empty = false;
+         lock.unlock();
+      }
+      /**
+       * Leaves a track.
+       * Sends a signal to the track, telling it that it is now empty.
+       */
+      public void leave(){
+         lock.lock();
+         empty = true;
+         critical.signal();
+         lock.unlock();
+      }
+      /**
+       * Tries to enter a track.
+       * if the track is empty, send enter(). 
+       * @return false if the track is taken.
+       * @return true if the track is empty.
+       */
+      public boolean tryEnter() throws InterruptedException {
+         boolean enterd = false;
+         if(empty){
+            enterd = true;
+            enter();
+         }
+         return enterd;
       }
    }
- public class Monitor{
-	private boolean occupated;
-    	private final ReentrantLock lock = new ReentrantLock();
-   	private Condition empty = lock.newCondition();
-
-	 public Monitor() {
-        this.occupated = false;
-    }
-
-    public void enter() {
-        lock.lock();
-        try {
-            if (this.occupated) {
-                this.empty.await();
-            }
-            occupated = true;
-        } catch (InterruptedException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    public void leave() {
-        lock.lock();
-        try {
-            this.empty.signal();
-            occupated = false;
-        } finally {
-            lock.unlock();
-        }
-    }
-    public boolean tryEnter() {
-        boolean ret;
-
-        lock.lock();
-        try {
-            if(!this.occupated) {
-                occupated = true;
-                ret = true;
-            } else {
-                ret = false;
-            }
-        } finally {
-            lock.unlock();
-        }
-        
-        return ret;
-    }
-
-	}
 }
 
