0c3d2acd209b2b57807b89aa4ca5af4d74452014
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2014-02-26 15:17:07 +0100

    implemented channels using a process for each channel

--- cchat/server.erl
+++ cchat/server.erl
@@ -32,17 +32,55 @@ loop(St, {disconnect, From, _Nick}) ->
 		
 loop(St, {join, From, _Channel, _Nick}) ->
         case lists:member(_Channel, St#server_st.channels) of
-            false -> {ok, St#server_st{channels = [_Channel | St#server_st.channels]}};
-            %nedan är fel
-            true -> {user_already_connected, St}
+            false ->
+                 Pid = spawn(fun() -> channel([_Nick]) end),
+                 register(list_to_atom(_Channel), Pid), 
+                 Pid ! {join, self(),  _Nick},
+                 {ok, St#server_st{channels = [_Channel | St#server_st.channels]}};
+            true -> 
+                 Reply = whereis(list_to_atom(_Channel)) ! {join, self(), _Nick},
+                 case Reply of
+                     ok -> 
+                        {ok, St};
+                     user_already_joined -> 
+                        {{error, user_already_joined, "You are already connected to that channel!"}, St}
+
+                 end
         end;
-		%%%%%%TODO: ALLT%%%%%
 loop(St, {leave, From, _Channel, _Nick}) ->
-		%%%%%%TODO: ALLT%%%%%
-    {ok, St};
-		%%%%%%TODO%%%%%
+     case whereis(list_to_atom(_Channel)) ! {leave, self(), _Nick} of
+         ok -> 
+            {ok, St};
+         user_not_joined -> 
+            {{error, user_not_joined, "You are not in that channel!"}, St}
+     end;
+
+        
 loop(St,{msg_from_GUI, From, Ref, _Channel, _Msg}) ->
     {ok, St}.
 		%%%%%%TODO%%%%%
+
+channel(Nicks) -> 
+    receive 
+       {join, From, Nick} -> 
+            case lists:member(Nick, Nicks) of
+                true -> 
+                    From ! user_already_joined,
+                    channel(Nicks);
+                false -> 
+                    channel([Nick | Nicks]),
+                    From ! ok
+            end;
+       {leave, From, Nick} ->
+            case lists:member(Nick, Nicks) of
+                false -> 
+                    From ! user_not_joined,
+                    channel(Nicks);
+                true -> 
+                    channel(lists:delete(Nick)),
+                    From ! ok
+            end
+    end.
+
 initial_state(_Server) ->
     #server_st{name = _Server, users = [], nicks = [], channels=[]}.
